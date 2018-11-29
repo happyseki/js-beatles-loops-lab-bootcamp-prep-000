8587dd602119024d187a9f100210c1e36cfe1e07
Elaina <elainaseki@gmail.com>
2018-11-29 14:54:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return array;
 }
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+function johnLennonFacts(){
+  var i=0;
+  while(i<facts.length){
+    i++;
+  }
+      var array=[];
+    array.push("facts[i] + \'!!!\'");
+    return array;
+}
