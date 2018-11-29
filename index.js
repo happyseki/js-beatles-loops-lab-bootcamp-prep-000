89ab8c9d541f0943c17d4f8fd6ce6953a5136122
Elaina <elainaseki@gmail.com>
2018-11-29 06:36:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
     const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
     const array = [];
@@ -7,3 +8,25 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return array;
 }
+=======
+// add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+    const array=[];
+function theBeatlesPlay(musicians, instruments){
+  for(var i=0;i<musicians.length;i++){
+       for(var j =0; j<instruments.length; j++){
+                     if(i===j){
+                      
+  var string=`${musicians[i]} plays ${instruments[i]}`;
+  array.push(string);
+  return array;
+ 
+  
+              }
+           }
+         }
+   }
+ 
+  
+>>>>>>> 7aec4d832bf81d9d80322d18c33104a02c80ac0a
