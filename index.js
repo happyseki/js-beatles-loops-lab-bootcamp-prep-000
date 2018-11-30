bb290f013c5d2013e7450d16f186bedd319c1e10
Elaina <elainaseki@gmail.com>
2018-11-30 05:34:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
     const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
     const array = [];
@@ -28,3 +29,26 @@ function iLoveTheBeatles(num){
   }while(num<15);
   return array;
 }
+=======
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+    const array = [];
+function theBeatlesPlay(musicians, instruments) {
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+function johnLennonFacts(){
+  while(i<facts.length){
+    
+  }
+}
+>>>>>>> ea76210f5595ff5ea282c1fe4d57ac38e133d971
